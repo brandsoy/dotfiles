9e5e5ff90ec1e53882885d9aac93599b2cf63a96
Mattis Brandsøy <mattis.brandsoy@icloud.com>
2024-11-19 22:22:58 +0100

    optimize

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,7 +22,7 @@ config.colors = {
 	-- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 -- config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
 
