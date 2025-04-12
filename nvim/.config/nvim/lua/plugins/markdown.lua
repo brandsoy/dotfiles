4be26e54c3689805803ac678001c58e5319c4ac9
Mattis Brandsøy <mattis.brandsoy@icloud.com>
2025-04-12 03:15:54 +0200

    wip

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -13,4 +13,12 @@ return {
       }
     end,
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
