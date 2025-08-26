f7cfc5607a45738c7dc6253f88576c5b2abccf61
Mattis Brandsøy <mattis.brandsoy@icloud.com>
2025-08-26 14:15:03 +0200

    partial arch merge

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,26 @@
+-- ================================================================================================
+-- TITLE : melange-nvim
+-- ABOUT : A subtle, warm colorscheme for Neovim inspired by Sublime Text's Melange theme.
+-- LINKS :
+--   > github : https://github.com/savq/melange-nvim
+-- ================================================================================================
+
+-- return {
+-- 	"savq/melange-nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd("colorscheme melange")
+-- 	end,
+-- }
+
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
+}
