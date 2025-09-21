0452382b6a3f7fda72f16ad5e90bfd140ca7c950
Mattis Brandsøy <mattis.brandsoy@icloud.com>
2025-09-21 19:33:38 +0200

    feat(nvim): configure mini.move and resolve keymap conflicts
    Configure the 'mini.move' plugin to use '<A-j>' and '<A-k>' for moving lines and selections, aligning with existing user preferences.
    
    Removes the previous custom keymaps for line and selection movement from 'lua/core/keymaps.lua' to avoid conflicts and centralize the functionality within the 'mini.move' plugin. This ensures a cleaner and more maintainable keymap configuration.

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,67 +1,153 @@
--- ================================================================================================
 -- TITLE: NeoVim keymaps
 -- ABOUT: sets some quality-of-life keymaps
 -- ================================================================================================
+
+local function map(mode, lhs, rhs, desc, opts)
+	local options = { silent = true, desc = desc }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
+end
+
+local function notify_option(label, enabled)
+	local ok = vim.notify ~= nil
+	if not ok then
+		return
+	end
+	vim.notify(string.format("%s %s", label, enabled and "enabled" or "disabled"), vim.log.levels.INFO, { title = "Neovim" })
+end
+
+local function bufremove_delete(buf, force)
+	local ok, plugin = pcall(require, "mini.bufremove")
+	if not ok then
+		return false
+	end
+	plugin.delete(buf, force)
+	return true
+end
 
 ------------------------------------------------------------------------------
 --- Normal Mode
 ------------------------------------------------------------------------------
 
 -- Disable Space bar since it'll be used as the leader key
-vim.keymap.set("n", "<leader>", "<nop>")
+map("n", "<leader>", "<nop>", "Disable bare leader key")
 
 -- Center screen when jumping
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+map("n", "n", "nzzzv", "Next search result (centered)")
+map("n", "N", "Nzzzv", "Previous search result (centered)")
+map("n", "<C-d>", "<C-d>zz", "Half page down (centered)")
+map("n", "<C-u>", "<C-u>zz", "Half page up (centered)")
+
+-- Quick buffer switch
+map("n", "<leader><leader>", "<Cmd>b#<CR>", "Switch to last buffer")
+
+-- Clear search highlight
+map("n", "<leader>h", "<Cmd>nohlsearch<CR>", "Clear search highlight")
 
 -- Buffer navigation
-vim.keymap.set("n", "<leader>bn", "<Cmd>bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>bp", "<Cmd>bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<leader>bn", "<Cmd>bnext<CR>", "Next buffer")
+map("n", "<leader>bp", "<Cmd>bprevious<CR>", "Previous buffer")
+map("n", "<leader>bd", function()
+	if not bufremove_delete(0, false) then
+		vim.cmd.bdelete()
+	end
+end, "Close current buffer")
+
+map("n", "<leader>bo", function()
+	local current = vim.api.nvim_get_current_buf()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+			if not bufremove_delete(buf, false) then
+				pcall(vim.api.nvim_buf_delete, buf, {})
+			end
+		end
+	end
+end, "Close other buffers")
+map("n", "<leader>br", "<Cmd>edit!<CR>", "Revert buffer from disk")
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+map("n", "<C-h>", "<C-w>h", "Move to left window")
+map("n", "<C-j>", "<C-w>j", "Move to bottom window")
+map("n", "<C-k>", "<C-w>k", "Move to top window")
+map("n", "<C-l>", "<C-w>l", "Move to right window")
 
 -- Splitting & Resizing
-vim.keymap.set("n", "<leader>sv", "<Cmd>vsplit<CR>", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>sh", "<Cmd>split<CR>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<C-Up>", "<Cmd>resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", "<Cmd>resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", "<Cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+map("n", "<leader>sv", "<Cmd>vsplit<CR>", "Split window vertically")
+map("n", "<leader>sh", "<Cmd>split<CR>", "Split window horizontally")
+map("n", "<leader>sx", "<Cmd>close<CR>", "Close current window")
+map("n", "<leader>se", "<C-w>=", "Equalize window sizes")
+map("n", "<C-Up>", "<Cmd>resize +2<CR>", "Increase window height")
+map("n", "<C-Down>", "<Cmd>resize -2<CR>", "Decrease window height")
+map("n", "<C-Left>", "<Cmd>vertical resize -2<CR>", "Decrease window width")
+map("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", "Increase window width")
 
 -- Better J behavior
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+map("n", "J", "mzJ`z", "Join lines and keep cursor position")
+
+
 
 -- Quick config editing
-vim.keymap.set("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
+map("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", "Edit Neovim config")
+
+-- Saving and quitting
+map("n", "<leader>ww", "<Cmd>write<CR>", "Save current buffer")
+map("n", "<leader>wa", "<Cmd>wall<CR>", "Save all buffers")
+map("n", "<leader>wq", "<Cmd>wq<CR>", "Save and quit window")
+map("n", "<leader>qq", "<Cmd>confirm qall<CR>", "Quit Neovim")
+map("n", "<leader>qQ", "<Cmd>qall!<CR>", "Force quit Neovim")
+
+-- Clipboard helpers
+map({ "n", "v" }, "<leader>y", '"+y', "Yank to system clipboard")
+map("n", "<leader>Y", '"+Y', "Yank line to system clipboard")
+map({ "n", "v" }, "<leader>p", '"+p', "Paste from system clipboard")
+
+-- Option toggles
+map("n", "<leader>un", function()
+	vim.wo.relativenumber = not vim.wo.relativenumber
+	notify_option("Relative number", vim.wo.relativenumber)
+end, "Toggle relative line numbers")
+
+map("n", "<leader>uc", function()
+	vim.wo.cursorline = not vim.wo.cursorline
+	notify_option("Cursor line", vim.wo.cursorline)
+end, "Toggle cursor line")
+
+map("n", "<leader>uw", function()
+	vim.wo.wrap = not vim.wo.wrap
+	notify_option("Line wrap", vim.wo.wrap)
+end, "Toggle soft wrap")
+
+map("n", "<leader>us", function()
+	vim.wo.spell = not vim.wo.spell
+	notify_option("Spell check", vim.wo.spell)
+end, "Toggle spell checking")
 
 --------------------------------------------------------------------------
 --- Visual Mode
 --------------------------------------------------------------------------
 -- Disable Space bar since it'll be used as the leader key
-vim.keymap.set("v", "<leader>", "<nop>")
+map("v", "<leader>", "<nop>", "Disable bare leader key")
 
 -- Better indenting in visual mode
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+map("v", "<", "<gv", "Indent left and reselect")
+map("v", ">", ">gv", "Indent right and reselect")
 
-vim.keymap.set("v", "<leader>mw", function()
+
+
+map("v", "<leader>mw", function()
 	local s = vim.fn.getreg("v") -- selected text
 	vim.cmd([[normal! gv]]) -- reselect
 	vim.cmd([[normal! c[]()]]) -- replace with []
 	vim.api.nvim_put({ s }, "c", true, true) -- put selection inside []
 	vim.cmd([[normal! F[a]]) -- move cursor into ()
-end, { desc = "Wrap selection in markdown link" })
+end, "Wrap selection in markdown link")
 
 -- Insert a markdown todo checkbox
-vim.keymap.set("n", "<leader>mt", "0i- [ ] <Esc>", { desc = "Insert markdown todo" })
-vim.keymap.set("v", "<leader>mt", ":s/^/- [ ] /<CR>:noh<CR>", { desc = "Make lines markdown todos" })
-vim.keymap.set("n", "<leader>mx", function()
+map("n", "<leader>mt", "0i- [ ] <Esc>", "Insert markdown todo")
+map("v", "<leader>mt", ":s/^/- [ ] /<CR>:noh<CR>", "Make lines markdown todos")
+map("n", "<leader>mx", function()
 	local line = vim.api.nvim_get_current_line()
 	if line:match("%[ %]") then
 		line = line:gsub("%[ %]", "[x]", 1)
@@ -69,14 +155,26 @@ vim.keymap.set("n", "<leader>mx", function()
 		line = line:gsub("%[x%]", "[ ]", 1)
 	end
 	vim.api.nvim_set_current_line(line)
-end, { desc = "Toggle markdown todo checkbox" })
+end, "Toggle markdown todo checkbox")
 -- Insert hyperlinks
-vim.keymap.set("n", "<leader>ml", "i[]()<Esc>F[a", { desc = "Insert markdown link" })
-vim.keymap.set("n", "<leader>mL", "i[]()<Esc>F(a<C-r>+<Esc>F[a", { desc = "Insert buffered markdown link" })
+map("n", "<leader>ml", "i[]()<Esc>F[a", "Insert markdown link")
+map("n", "<leader>mL", "i[]()<Esc>F(a<C-r>+<Esc>F[a", "Insert buffered markdown link")
 
 --------------------------------------------------------------------------
 --- Interactive Mode
 --------------------------------------------------------------------------
 
 -- JK Escape
-vim.keymap.set("i", "jk", [[<C-\><C-n>]])
+map("i", "jk", [[<C-\><C-n>]], "Exit insert mode")
+
+
+
+------------------------------------------------------------------------------
+--- Terminal Mode
+------------------------------------------------------------------------------
+
+map("t", "<Esc>", [[<C-\><C-n>]], "Exit terminal mode")
+map("t", "<C-h>", [[<C-\><C-n><C-w>h]], "Terminal: move to left window")
+map("t", "<C-j>", [[<C-\><C-n><C-w>j]], "Terminal: move to bottom window")
+map("t", "<C-k>", [[<C-\><C-n><C-w>k]], "Terminal: move to top window")
+map("t", "<C-l>", [[<C-\><C-n><C-w>l]], "Terminal: move to right window")
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -3,21 +3,59 @@ return function()
 	vim.pack.add({
 		{ src = "https://github.com/stevearc/oil.nvim" },
 		{ src = "https://github.com/folke/which-key.nvim" },
+		{ src = "https://github.com/nvim-mini/mini.bufremove" },
+		{ src = "https://github.com/nvim-mini/mini.pairs" },
+		{ src = "https://github.com/rcarriga/nvim-notify" },
+		{ src = "https://github.com/nvim-mini/mini.ai" },
+		{ src = "https://github.com/nvim-mini/mini.move" },
 	})
 
 	-- Oil file manager
 	require("oil").setup()
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+	-- Buffer remove helper
+	require("mini.bufremove").setup()
+
+	require("mini.ai").setup()
+
+	require("mini.move").setup({
+		mappings = {
+			down = "<A-j>",
+			up = "<A-k>",
+			line_down = "<A-j>",
+			line_up = "<A-k>",
+		},
+	})
+
+	-- Autopairs
+	require("mini.pairs").setup({
+		modes = { insert = true, command = false, terminal = false },
+	})
+
 	-- Which-key for keymap help
 	require("which-key").setup()
 	local wk = require("which-key")
 	wk.add({
-		{ "<leader>m", group = "Markdown" },
-		{ "<leader>f", group = "FZF" },
+		{ "<leader>b", group = "Buffers" },
+		{ "<leader>d", group = "Debug" },
+		{ "<leader>f", group = "Find" },
 		{ "<leader>l", group = "LSP" },
 		{ "<leader>lq", desc = "Diagnostics to Location List" },
-		{ "<leader>d", group = "Debug" },
+		{ "<leader>m", group = "Markdown" },
+		{ "<leader>q", group = "Quit" },
+		{ "<leader>s", group = "Splits" },
+		{ "<leader>u", group = "Toggles" },
+		{ "<leader>w", group = "Write" },
 	})
-end
 
+	-- Notifications
+	local notify = require("notify")
+	notify.setup({
+		background_colour = "#1f1f28",
+		stages = "fade_in_slide_out",
+		timeout = 2000,
+		render = "compact",
+	})
+	vim.notify = notify
+end
