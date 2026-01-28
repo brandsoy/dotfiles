85c605faef4d94f22f8d5c21ec53f4429ec1ef37
Mattis Brandsøy <mattis.brandsoy@pm.me>
2026-01-28 22:56:05 +0100

    wip debian

--- home/config/.config/nvim/lua/plugins/treesitter.lua
+++ home/config/.config/nvim/lua/plugins/treesitter.lua
@@ -41,8 +41,8 @@ return {
 				},
 			},
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		-- config = function(_, opts)
+		-- 	require("nvim-treesitter.configs").setup(opts)
+		-- end,
 	},
 }
